14529a30433f11c243d74a41140446e62ca3f1c4
Ttdne <86353412+Ttdne@users.noreply.github.com>
2023-11-26 11:14:12 +0700

    hehe

--- src/server.js
+++ src/server.js
@@ -1,28 +1,21 @@
+require('dotenv').config({ path: 'D:/CPP/Learn-NodeJS/.env' })
 const express = require('express') //common js
 //const { hostname } = require('os')
-require('dotenv').config({ path: 'D:/CPP/Learn-NodeJS/src/.env' })
 
 // import
 console.log(process.env);
 const path = require('path')
+const configViewEngine = require('./config/viewengine')
+const webRoutes = require('./routes/web')
 const app = express() //app express
 const port = process.env.PORT;  //port 
 const hostname = process.env.HOST_NAME;
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+
+//config template engine
+configViewEngine(app);
 
 //khai báo route
-app.get('/', (req, res) => {
-    //res.send('Hello World!')
-    res.render('sample.ejs')
-})
-app.get('/abc', (req, res) => {
-    res.send('check ABC')
-})
-
-app.get('/baba', (req, res) => {
-    res.send('<h1>check ABC</h1>')
-})
+app.use('/', webRoutes)
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
 })
